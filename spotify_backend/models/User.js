d18f86f9c4944674dace0562819738d1e10a3b65
Ruchi-codes04 <p.ruchika142003@gmail.com>
2024-12-28 17:12:44 +0530

    Add files via upload

---
+++ spotify_backend/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required:true,
+    },
+    password: {
+        type: String,
+        required: true,
+        private: true,
+    },
+    lastName: {
+        type: String,
+        required:false,
+    },
+    email: {
+        type: String,
+        required:true,
+    },
+    username: {
+        type: String,
+        required:true,
+    },
+    likedSongs: {
+        // change this to array later
+        type: String,
+        default:"",
+    },
+    likedPlaylists: {
+         // change this to array later
+        type: String,
+        default:"",
+    },
+    subscribedArtists:{
+        // change this to array later
+       type: String,
+       default:"",
+   },
+   // can be added more fields as required...
+    
+    
+});
+
+
+
+const UserModel = mongoose.model("User", User);
+module.exports = UserModel;
